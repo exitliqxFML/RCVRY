afb14a9d7fdc1f822bf9b50c64943d08650e52a5
exitliqxFML <omarvazquez2203@icloud.com>
2025-05-31 23:52:11 -0500

    Update WalletContext.js

--- src/WalletContext.js
+++ src/WalletContext.js
@@ -10,7 +10,7 @@ import {
   PhantomWalletAdapter
 } from "@solana/wallet-adapter-wallets";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const network = "https://api.mainnet-beta.solana.com";
 
